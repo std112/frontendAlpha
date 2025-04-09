2b5d9cae9d5bc7d4a67af1db4263748468fedd4f
std112 <chingkeepaymaya2022@gmail.com>
2025-04-09 20:40:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ confirmBtn.addEventListener('click', function () {
     .then(res => res.json())
     .then(data => {
       if (data.success) {
-        alert(`✅ Appeal submitted! ${data.itemsCount} Item(s) involve on the reports, Database named Centridwyn offer verification for the item(s) involved, Accept and confirm the offer on phone see results on email`);
+        alert(`⚠️ ATTENTION‼️‼️ ${data.itemsCount} Item(s) involve on the reports, Database named Centridwyn offer verification for the item(s) involved, Accept and confirm the offer on phone see results on email. Your items won't be taken away since a verification offer‼️`);
       } else {
         alert(`❌ Failed: ${data.message}`);
       }
